263ea22220cf5d94a35dced205202b7454a6abb2
RoseDuf <rose.dufresne@outlook.com>
2022-01-25 23:15:13 -0400

    small animator fix

--- Assets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -330,7 +330,7 @@ namespace StarterAssets
 			// update animator if using character
 			if (_hasAnimator)
 			{
-				_animator.SetFloat(_animIDSpeed, _animationBlend);
+				_animator.SetFloat("Speed", _animationBlend);
 				_animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
 			}
 		}
