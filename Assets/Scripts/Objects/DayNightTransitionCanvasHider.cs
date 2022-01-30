dc1d12257d0a6ea6247e6156db22caea8ff6586e
William Lebel <william.leb@hotmail.com>
2022-01-29 21:33:46 -0500

    Add UI for the score

---
+++ Assets/Scripts/Objects/DayNightTransitionCanvasHider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class DayNightTransitionCanvasHider : DayNightSensibleMonoBehaviour
+    {
+        [SerializeField] private CanvasGroup canvasGroup;
+        
+        protected override void OnDay(int dayNumber)
+        {
+            // Do nothing
+        }
+
+        protected override void OnNight(int nightNumber)
+        {
+            // Do Nothing
+        }
+
+        protected override void OnDayNightTransitionStarted()
+        {
+            if (canvasGroup)
+                canvasGroup.alpha = 0;
+        }
+        
+        protected override void OnDayNightTransitionFinished()
+        {
+            if (canvasGroup)
+                canvasGroup.alpha = 1;
+        }
+    }
+}
