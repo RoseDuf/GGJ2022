8ebf1b27108fd14751a51d700e599cb82dc4825c
William Lebel <william.leb@hotmail.com>
2022-01-29 19:14:54 -0500

    Add setting system and settings for this scoring system

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Game
+{
+    public class ScoreManager : Singleton<ScoreManager>
+    {
+        private int score = 0;
+
+        public int Score => score;
+
+        public void AddScore(int scoreToAdd) => score += scoreToAdd;
+    }
+}
---
+++ Assets/Scripts/Scriptables/GlobalSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu(menuName = "Settings/Global Settings")]
+    public class GlobalSettings : ScriptableObject
+    {
+        [Header("Points")] 
+        [SerializeField] private int pointsForSmallGuy = 100;
+        [SerializeField] private int pointsForBigGuy = 250;
+        [SerializeField] [Tooltip("Add this percent to the gained score for each combo")] private float comboScoreMultiplier = 0.1f;
+
+        public int PointsForSmallGuy => pointsForSmallGuy;
+        public int PointsForBigGuy => pointsForBigGuy;
+        public float ComboScoreMultiplier => comboScoreMultiplier;
+    }
+}
