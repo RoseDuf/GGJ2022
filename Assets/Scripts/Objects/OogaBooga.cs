bff13ec63167af430aa5b7ad64982ca20a18bd3d
Jean Lamarre <jonny.lamarre@hotmail.com>
2022-02-10 11:59:08 -0500

    dont wowwy about it

---
+++ Assets/Scripts/Objects/OogaBooga.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Game
+{
+    [RequireComponent(typeof(Animator))]
+    public class OogaBooga : MonoBehaviour
+    {
+        private Animator SplashAnimator;
+        private void Start()
+        {
+            SplashAnimator = GetComponent<Animator>();
+            StartCoroutine(OogaBoogaRoutine());
+        }
+
+        private IEnumerator OogaBoogaRoutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(1f);
+                Debug.Log("OOGA BOOGA");
+                SplashAnimator.SetTrigger("SplashTrigger");
+                SoundSystem.Instance.PlayOogaBoogaSplashSound();
+            }
+        }
+    }
+}
